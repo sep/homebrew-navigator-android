bbc2dfa8723d00a4c1951e18b165d5cd18ab4323
Chris Halvorson <crhalvorson@sep.com>
2012-08-04 19:33:53 -0400

    Bluetooth stability fixes.  Now not going totally crazy when connection is lost.  Code cleanup.

--- src/com/example/homebrewnavigator/TemperatureService.java
+++ src/com/example/homebrewnavigator/TemperatureService.java
@@ -6,17 +6,14 @@ import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Set;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Build;
 import android.os.IBinder;
 
 //Define an IntentService that lives (effectively) forever
@@ -27,15 +24,15 @@ import android.os.IBinder;
 public class TemperatureService extends NonStopIntentService {
 
 	private BluetoothSocket socket = null;
-	private BluetoothDevice[] bluetooth_devices = null;
+	
+	private BluetoothDevice device = null;
 	private OutputStream out = null;
 	private InputStream in = null;
-	
-	private boolean notificationFired = false;
 	
 	private boolean keep_running = false;
 	
 	private float temperature = 0;
+	private boolean connected = false;
 	
 	private NotificationManager  notiManager;
 	
@@ -68,38 +65,12 @@ public class TemperatureService extends NonStopIntentService {
 	private void fireNotification(String temp)
 	{
 		int icon = R.drawable.tempnot;
-		String text = temp;
-		long when = System.currentTimeMillis();
-		
 		Context context = getApplicationContext();
 		String contentTitle = "Temperature notification";
 		String contentText = "Your current temperature is " + temp;
-		Intent notificationIntent = new Intent(this, TemperatureService.class);
-		int flag = Notification.FLAG_ONGOING_EVENT;
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, flag);
-
-		
-		
-		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
-		{
-			Notification noti = new Notification(icon, temp, when);
-			noti.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-			notiManager.notify(TEMP_NOTIF_ID, noti);
-		}
-		else
-		{
-			fireNotiApi11(context, contentTitle, contentText, icon);
-		}
-		
-		
-	}
-	
-	@TargetApi(11)
-	private void fireNotiApi11(Context context, String title, String text, int icon)
-	{
 		Notification.Builder builder = new Notification.Builder(context)
-        .setContentTitle(title)
-        .setContentText(text)
+        .setContentTitle(contentTitle)
+        .setContentText(contentText)
         .setSmallIcon(icon);
 		
 		Notification noti = builder.getNotification();
@@ -116,6 +87,8 @@ public class TemperatureService extends NonStopIntentService {
         
         //We should never actually use the below value
         String[] devices_strings = { "No Adapter Found" };
+        
+        BluetoothDevice[] bluetooth_devices;
         
         if (adapter != null)
         {
@@ -139,7 +112,7 @@ public class TemperatureService extends NonStopIntentService {
             try
             {
             	
-            	BluetoothDevice device = adapter.getRemoteDevice(bluetooth_devices[0].getAddress());
+            	device = adapter.getRemoteDevice(bluetooth_devices[0].getAddress());
             	//Connect to SPP service - build-in method is apparently broken.  this reflection is a workaround.
             	Method m;
             	m = device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
@@ -182,6 +155,8 @@ public class TemperatureService extends NonStopIntentService {
 	public void onDestroy()
 	{
 		super.onDestroy();
+		
+		keep_running = false;
 		try
 	    {
 	      if (in != null) in.close();
@@ -195,8 +170,18 @@ public class TemperatureService extends NonStopIntentService {
 	
 	@Override
 	public IBinder onBind(Intent intent) {
-		onHandleIntent(intent);
+		super.onBind(intent);
+		
 		return temperatureBinder;
+		
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		startBluetoothService();
+		startTempThread(in);
 	}
 	
 	public float GetLastTemperature()
@@ -206,7 +191,7 @@ public class TemperatureService extends NonStopIntentService {
 	
 	public boolean isConnected()
 	{
-		return socket != null;
+		return connected;
 	}
 	
 	final Runnable runnable = new Runnable()
@@ -219,9 +204,22 @@ public class TemperatureService extends NonStopIntentService {
 		      {
 		        try
 		        {
+		        	
+		        	if(socket == null)
+		        	{
+		        		AttemptBtConnection();
+		        		
+		        		//Wait for 10 seconds, breaking if something happens.  Wouldn't want to drain the battery too much.
+		        		for(int i=0; i<100; i++)
+		        		{
+		        			Thread.sleep(100);
+		        			if(!keep_running) break;
+		        		}
+		        	}
+		        	
 		        	String temp = ""; 
-		        	//allow breakout of loop while reading
-		        	while(keep_running && (ch = in.read() ) != 13 )
+		        	//allow break-out of loop while reading
+		        	while(keep_running && socket != null && (ch = in.read() ) != 13 )
 		            {
 		        		temp = temp + (char)ch;
 		            }
@@ -230,32 +228,51 @@ public class TemperatureService extends NonStopIntentService {
 		        	try
 		        	{
 		        		temperature = Float.parseFloat(temp);
+		        		connected = true;
 		        		fireNotification(temp);
 		        	}
 		        	catch(Exception e)
 		        	{
 		        		temperature = 0.0f;
+		        		connected = false;
 		        	}
 		          }
 		        catch (Exception e) {
 		        	System.out.println("BlueTemp problem: "+e.toString());
 		        	try { socket.close(); } catch(Exception ex1) { System.out.println("Shutdown problem"); } finally 
 		        	{
+		        		connected = false;
 		        		socket = null;
 		        		temperature = 0;
-		        		keep_running = false;
 		        	}
 		        }
 	      }
 	    }
 	  };
 
+	  private void AttemptBtConnection()
+	  {
+		  if(device == null) return;
+		  try
+		  {
+			 Method m;
+			 m = device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
+			 socket = (BluetoothSocket)m.invoke(device, Integer.valueOf(1));
+			 //Attempt to connect.
+			 socket.connect();
+			 out = socket.getOutputStream();
+			 in = socket.getInputStream();
+		  }
+		  catch(Exception e)
+		  {
+			  BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+			  adapter.cancelDiscovery();
+		  }
+	  }
 
 	  @Override
 	  protected void onHandleIntent(Intent intent) {
-		  startBluetoothService();
-		  // TODO Auto-generated method stub
-		  startTempThread(in);
+		  
 	  }
 
 }
