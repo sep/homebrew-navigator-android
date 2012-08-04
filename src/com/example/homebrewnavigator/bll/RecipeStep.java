65ca3bedf3065da27307655025f5cef464218a68
unknown <rmnowadly@.net.sep.com>
2012-08-04 11:25:13 -0400

    adding unit test project

---
+++ src/com/example/homebrewnavigator/bll/RecipeStep.java
@@ -0,0 +1,20 @@
+package com.example.homebrewnavigator.bll;
+
+import java.util.GregorianCalendar;
+
+public class RecipeStep {
+	private int doSomethingInMinutes;
+	private String instruction;
+	private RecipeStepCriteria type;
+	public RecipeStepCriteria getCriteria() {
+		return type;
+	}
+	public String getInstruction() {
+		return instruction;
+	}
+	
+	
+	
+	
+	
+}
