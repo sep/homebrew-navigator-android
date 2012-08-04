3bde5c56deb5602cf5f1f77e2fb2a716053b4ca8
unknown <rmnowadly@.net.sep.com>
2012-08-04 10:33:23 -0400

    One test passed

---
+++ src/com/example/homebrewnavigator/bll/Recipe.java
@@ -0,0 +1,14 @@
+package com.example.homebrewnavigator.bll;
+
+public class Recipe {
+
+	private String name;
+	public String getName() {		
+		return name;
+	}
+
+	public void setName(String daName) {
+		name = daName;		
+	}
+
+}
